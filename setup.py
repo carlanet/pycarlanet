a1baa6c7a120afbd3bf806a807e7d11c3b8d922f
jaivra <jaivra.info@gmail.com>
2023-03-04 11:47:01 +0100

    added python version requirements for installing the library

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import find_packages, setup
+import sys
 
 setup(
     name="pycarlanet",
     packages=find_packages(include=['pycarlanet', 'pycarlanet.utils']),
-    version="0.8.0-alpha",
+    version="0.8.1-alpha",
     description='Carla and Omnet++ co-simulation library',
     author="Valerio Cislaghi, Christian Quadri",
     license='MIT',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,<3.9',
     install_requires=[
         'pyzmq==23.2.1',
         'carla==0.9.13'
@@ -16,7 +18,7 @@ setup(
     },
     setup_requires=['pytest-runner'],
     tests_require=[
-        'pytest==4.4.1'
+        'pytest==7.2.1'
     ],
     test_suite='tests'
     # setup_requires=['pytest-runner'],
