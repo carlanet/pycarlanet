645b31ef8e7c5793e9e049c205c7045341b7e79f
jaivra <jaivra.info@gmail.com>
2023-03-04 15:43:47 +0100

    replaced travis with github workflow

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ import sys
 setup(
     name="pycarlanet",
     packages=find_packages(include=['pycarlanet', 'pycarlanet.utils']),
-    version="0.8.2-alpha",
-    description='Carla and Omnet++ co-simulation library',
+    version="0.8.5-alpha",
+    long_description="""Carla and Omnet++ co-simulation library""",
+    long_description_content_type='text/markdown',
     author="Valerio Cislaghi, Christian Quadri",
     license='MIT',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,<3.9',
