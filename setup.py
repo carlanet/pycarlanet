e9a34a2b5498372f204e52ce72e3b62cb8bc5478
Stefano <stefanoditerlizzi96@gmail.com>
2024-06-09 15:28:33 +0200

    setup update

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from pathlib import Path
 
 setup(
     name="pycarlanet",
-    packages=find_packages(include=['pycarlanet', 'pycarlanet.utils']),
-    version="0.9.0-alpha",
+    packages=find_packages(include=['pycarlanet', 'pycarlanet.utils', 'pycarlanet.enum', 'pycarlanet.listeners']),
+    version="1.0-alpha",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type='text/markdown',
-    author="Valerio Cislaghi, Christian Quadri",
+    author="Valerio Cislaghi, Christian Quadri, Stefano di Terlizzi",
     license='MIT',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,<3.9',
     install_requires=[
         'pyzmq==23.2.1',
-        'carla==0.9.13'
+        'carla==0.9.*'
     ],
     project_urls={
         'Source': 'https://github.com/jaivra/pycarlanet',
