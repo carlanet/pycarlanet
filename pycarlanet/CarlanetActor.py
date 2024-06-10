d3de1e78c2b733a8a41a77877a8cfc399c52d7e5
Stefano di Terlizzi <stefanoditerlizzi96@gmail.com>
2024-06-10 14:48:23 +0200

    add ActorType, remove net _active

--- pycarlanet/CarlanetActor.py
+++ pycarlanet/CarlanetActor.py
@@ -1,5 +1,6 @@
 import abc
 import carla
+from enum import Enum
 
 from pycarlanet.utils import preconditions
 
@@ -7,19 +8,31 @@ from pycarlanet.utils import preconditions
 # NOTE: you can call each method of carla.Actor on this class, but if you want to pass an object of type
 # CarlanetActor to a methods defined in carla you have to pass the attribute carla_actor
 # because carla can't see this class
+
+class ActorType(Enum):
+    #TODO define module omnet for each type
+    Sensor = 'a'
+    Spectator = 'b'
+    Traffic_sign = 'c'
+    Traffic_light = 'd'
+    Vehicle = 'e'
+    Walkers = 'f'
+
 class CarlanetActor(abc.ABC):
 
-    def __init__(self, carla_actor: carla.Actor, alive: bool):
+    def __init__(self, carla_actor: carla.Actor, actor_type: ActorType):
         self._carla_actor = carla_actor
-        self._alive = alive
+        self._actor_type = actor_type
 
-    @preconditions('_carla_actor')
-    def __getattr__(self, *args):
-        return self._carla_actor.__getattribute__(*args)
+    #@preconditions('_carla_actor')
+    #def __getattr__(self, *args):
+    #    return self._carla_actor.__getattribute__(*args)
 
-    def apply_command(self, command):
-        carla.command.ApplyVehicleControl(self.id, command)
+    #def apply_command(self, command):
+    #    carla.command.ApplyVehicleControl(self.id, command)
 
     @property
-    def alive(self):
-        return self._alive
+    def carla_actor(self): return self._carla_actor
+
+    @property
+    def actor_type(self): return self._actor_type
